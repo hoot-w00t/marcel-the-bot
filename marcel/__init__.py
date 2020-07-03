022b61388ca4382ce26a0f0ec75b3dafce81cc5a
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2020-07-03 17:13:22 +0200

    Switch to version 3.1.1

--- marcel/__init__.py
+++ marcel/__init__.py
@@ -19,4 +19,4 @@ from marcel.program import *
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = "3.1.0"
+__version__ = "3.1.1"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="marcel-the-bot",
-    version="3.1.0",
+    version="3.1.1",
     author="akrocynova",
     author_email="",
     description="Marcel the Discord Bot",
