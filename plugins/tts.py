53dac3d133dd91d71d85250dfc0a6b87715af03b
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2020-07-03 13:59:57 +0200

    Update Text To Speech plugin

--- plugins/tts.py
+++ plugins/tts.py
@@ -30,12 +30,14 @@ class MarcelPlugin:
     plugin_description = "Google Text To Speech plugin"
     plugin_author = "https://github.com/hoot-w00t"
     plugin_help = """`{prefix}say`/`{prefix}speak` [text] speaks your text in a voice channel using Google's TTS.
-    `{prefix}tts-lang` [lang] will change the Text To Speech language (`en` by default)"""
+    `{prefix}tts-lang` [lang] will change the Text To Speech language (`en` by default)
+    `{prefix}tts-langs` will display a list of the supported Text To Speech languages"""
 
     bot_commands = [
-        ("say", "tts_cmd"),
-        ("speak", "tts_cmd"),
-        ("tts-lang", "tts_lang_cmd")
+        ("say", "tts_cmd", "clean_command"),
+        ("speak", "tts_cmd", "clean_command"),
+        ("tts-lang", "tts_lang_cmd", "clean_command"),
+        ("tts-langs", "tts_langs_cmd", "clean_command")
     ]
 
     def __init__(self, marcel: Marcel):
@@ -54,9 +56,8 @@ class MarcelPlugin:
 
         return tmp_file
 
-    async def tts_cmd(self, message: discord.Message, args: list):
-        guild_settings = self.marcel.get_server_settings(message.guild)
-        lang = guild_settings.get("tts_lang", "en")
+    async def tts_cmd(self, message: discord.Message, args: list, **kwargs):
+        lang = kwargs.get("settings").get("tts_lang", "en")
         text = " ".join(args).strip()
 
         if len(text) > 0:
@@ -80,51 +81,84 @@ class MarcelPlugin:
                 message.channel,
                 message.author,
                 silent=True,
-                autoplay=True if len(mp.player_queue) > 0 else False)
+                autoplay=True if len(mp.player_queue) > 0 else False
+            )
 
-    async def tts_lang_cmd(self, message: discord.Message, args: list):
-        guild_settings = self.marcel.get_server_settings(message.guild)
+    async def tts_lang_cmd(self, message: discord.Message, args: list, **kwargs):
+        settings = kwargs.get("settings")
         new_lang = " ".join(args).strip()
 
         if len(new_lang) == 0:
             await message.channel.send(
                 embed=embed_message(
-                    "Text To Speech",
+                    "Text To Speech language is",
                     discord.Color.blue(),
-                    "Text To Speech language is: {}".format(
-                        self.langs.get(
-                            guild_settings.get("tts_lang", "en")
-                        )
-                    )
-                )
-            )
-            return
-
-        if not self.marcel.is_member_admin(message.author):
-            await message.channel.send(
-                embed=embed_message(
-                    "Only the server administrators have access to this command",
-                    discord.Color.dark_red()
-                )
+                    self.langs.get(settings.get("tts_lang", "en"))
+                ),
+                delete_after=settings.get("delete_after")
             )
             return
 
         lang_name = self.langs.get(new_lang)
         if lang_name:
-            guild_settings["tts_lang"] = new_lang
+            settings["tts_lang"] = new_lang
 
             await message.channel.send(
                 embed=embed_message(
-                    "Text To Speech",
+                    "Text To Speech language set to",
                     discord.Color.blue(),
-                    "Text To Speech language is now: {}".format(lang_name)
-                )
+                    lang_name
+                ),
+                delete_after=settings.get("delete_after")
             )
-        else:
-            await message.channel.send(
-                embed=embed_message(
-                    "Text To Speech",
-                    discord.Color.dark_red(),
-                    "Invalid lang"
+            return
+
+        new_lang = new_lang.lower()
+        for lang in self.langs:
+            if self.langs[lang].lower().startswith(new_lang):
+                settings["tts_lang"] = lang
+
+                await message.channel.send(
+                    embed=embed_message(
+                        "Text To Speech language set to",
+                        discord.Color.blue(),
+                        self.langs[lang]
+                    ),
+                    delete_after=settings.get("delete_after")
                 )
+                return
+
+        await message.channel.send(
+            embed=embed_message(
+                self.plugin_name,
+                discord.Color.dark_red(),
+                "Invalid lang"
+            ),
+            delete_after=settings.get("delete_after")
+        )
+
+    async def tts_langs_cmd(self, message: discord.Message, args: list, **kwargs):
+        part = 1
+        count = 0
+
+        embed = discord.Embed(color=discord.Color.blue())
+        embed.set_author(name="Text to speech langs (part {})".format(part))
+
+        for lang in self.langs:
+            embed.add_field(
+                name=self.langs[lang],
+                value=lang,
+                inline=True
             )
+            count += 1
+
+            if count >= 20:
+                count = 0
+                part += 1
+                await message.channel.send(embed=embed)
+
+                embed = discord.Embed(color=discord.Color.blue())
+                embed.set_author(name="Text to speech langs (part {})".format(part))
+
+        if count > 0:
+            await message.channel.send(embed=embed)
