410577e7d9baba8819b2a697846362049b77d1c1
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2020-07-22 12:25:16 +0200

    Version 3.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="marcel-the-bot",
-    version="3.1.1",
+    version="3.2.1",
     author="akrocynova",
     author_email="",
     description="Marcel the Discord Bot",
